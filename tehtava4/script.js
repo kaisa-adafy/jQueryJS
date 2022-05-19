16dcd909cbffa0256c01b17910887fc60099352c
Kaisa Koski <kaisa.koski@adafy.com>
2022-05-19 17:10:39 +0300

    2.4 almost ready

--- tehtava4/script.js
+++ tehtava4/script.js
@@ -1,9 +1,32 @@
-var url = "https://itunes.apple.com/lookup?id=1472380750&entity=album";
-var data = $.get(url);
-console.log(data);
+var url = "https://itunes.apple.com/search?term=darude";
 
+$().ready(function() {
+    $.ajax({
+    url: url,
+    jsonp: "callback",
+    dataType: "jsonp",
+    data: {
+        q: "select title,abstract,url from search.news where query=\"cat\"",
+        format: "json"
+    },
+    success: function( response ) {
+        console.log( response );
+        var tracks = response.results;
+        tracks.forEach(track => {
+            var name = track.collectionName;
+            var url = track.artworkUrl100;
+            addRecordCard(url,name);
+        });
+    }
+});
+});
 
-
-function makeCard(){
-
-}
+function addRecordCard(url, name){
+var recordCard ="<div class=\"card\" style=\"width: 12rem;\">" + 
+                "<img src=" + url + " class=\"card-img-top\" alt=\"Collection with record "+name+"\">"+
+                "<div class=\"card-body\">"+
+                "<h5 class=\"card-title\">"+name+"</h5>"+
+                "</div>"+
+                "</div>";
+$("#records").append(recordCard);
+};
