1f8f2660bbbc155a35cc37c24d4b9ffe3dd27c5d
Kaisa Koski <kaisa.koski@adafy.com>
2022-05-20 09:50:00 +0300

    2.4 fixes

--- tehtava4/script.js
+++ tehtava4/script.js
@@ -1,32 +1,37 @@
 var url = "https://itunes.apple.com/search?term=darude";
 
-$().ready(function() {
+$().ready(function () {
     $.ajax({
-    url: url,
-    jsonp: "callback",
-    dataType: "jsonp",
-    data: {
-        q: "select title,abstract,url from search.news where query=\"cat\"",
-        format: "json"
-    },
-    success: function( response ) {
-        console.log( response );
-        var tracks = response.results;
-        tracks.forEach(track => {
-            var name = track.collectionName;
-            var url = track.artworkUrl100;
-            addRecordCard(url,name);
-        });
-    }
-});
+        url: url,
+        jsonp: "callback",
+        dataType: "jsonp",
+        data: {
+            q: "select title,abstract,url from search.news where query=\"cat\"",
+            format: "json"
+        },
+        success: function (response) {
+            var tracks = response.results;
+            var records = [];
+            tracks.forEach(track => {
+                var name = track.collectionName;
+                var url = track.artworkUrl100;
+                if (!records.includes(name)) {
+                    addRecordCard(url, name);
+                    records.push(name);
+                }
+            });
+        }
+    });
 });
 
-function addRecordCard(url, name){
-var recordCard ="<div class=\"card\" style=\"width: 12rem;\">" + 
-                "<img src=" + url + " class=\"card-img-top\" alt=\"Collection with record "+name+"\">"+
-                "<div class=\"card-body\">"+
-                "<h5 class=\"card-title\">"+name+"</h5>"+
-                "</div>"+
-                "</div>";
-$("#records").append(recordCard);
+function addRecordCard(url, name) {
+    var recordCard = "<div class=\"card m-1\" style=\"width: 240px;\">" +
+        "<img src=" + url + " class=\"card-img-top\" alt=\"Collection with record " + name + "\">" +
+        "<div class=\"card-body\">" +
+        "<h5 class=\"card-title\">" + name + "</h5>" +
+        "</div>" +
+        "</div>";
+    $("#records").append(recordCard);
 };
+
+//
